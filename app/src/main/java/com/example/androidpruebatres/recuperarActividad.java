a5cfc084af40bf48431b35026f734efd371c5c9e
Joel Arias <joelariasparra3@gmail.com>
2022-12-20 03:25:05 -0300

    Primer commit, creacion views

---
+++ app/src/main/java/com/example/androidpruebatres/recuperarActividad.java
@@ -0,0 +1,14 @@
+package com.example.androidpruebatres;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class recuperarActividad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recuperar_actividad2);
+    }
+}
